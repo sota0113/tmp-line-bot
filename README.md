1244ad1d3a4cf78298f822dfa45fd27c095c99d2
Sota Nakanishi <AHA04462@jp.ibm.com>
2020-10-18 01:45:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# TMP line bot
+## Description
+内輪向けの筋トレ管理LINEボット  
+## Architecture
+メインロジックの[lambda_function.py](./lambda_function.py)はAWS Lambdaで動作。  
+現在DB不在。なるべくお金がかからないサービスを選定中。  
+
+## Deployment
+AWS Lambdaのpython3.xのデフォルト環境では不足するライブラリがあるため、必要なファイル一式をzipでアップロードする必要があります。   
+今回不足したreqstsライブラリは既に本repoに用意済みですが念の為以下に手順を記します。  
+```
+mkdir -p tmp-repo/package
+cd tmp-repo/package
+pip3 install requests --target .
+zip -r9 ../function.zip .
+```
+
+以下コマンドで`function.zip`を作成し、Lambdaに展開します。  
+```
+zip -g function.zip lambda_function.py
+```
+
+## 今後
+・DBの選定。  
+・回数カウントロジック。  
+・回数モニタリングと未達者へのアラート発報。  
+・自然言語処理。正規表現では限界。 
---
+++ lambda_function.py
@@ -0,0 +1,99 @@
+import json
+import os
+import re
+import requests
+import random
+
+auth = os.environ['AUTH']
+url = os.environ['ENDPOINT']
+
+def lambda_handler(event, context):
+    
+    print(event)
+    reply_token = event["events"][0]["replyToken"]
+    message = event["events"][0]["message"]["text"]
+
+    
+    match = bool(re.match(r".*完了.*", message))
+    if match == True:
+        return_msg = "「完了」の正規表現で拾っています。DDBが無いから記憶できない。すまん。"
+        message_to_line(reply_token,return_msg)
+        
+    #match = bool(re.match(r".*休み.*", message))
+    match = bool(re.match(r".*休み.*", message))
+    if match == True:
+        return_msg = "「休み」の正規表現で拾っています。DBが無いから記憶できない。すまん。"
+        message_to_line(reply_token,return_msg)
+        
+    match = bool(re.match(r".*バド.*", message))
+    if match == True:
+        items_one = ("了解","はいよ","おっけ","はーい")
+        items_two = ("","頑張れー","頑張ってください","プロテイン忘れずに","")
+        items_punc_one = ("","","。","！")
+        items_punc_two = ("","","。","！")
+        num_one = random.randint(0, 3)
+        num_two = random.randint(0, 4)
+        num_punc_one = random.randint(0, 2)
+        num_punc_two = random.randint(0, 2)
+        return_msg = items_one[num_one]+items_punc_one[num_punc_one]+items_two[num_two]+items_punc_two[num_punc_two]
+        message_to_line(reply_token,return_msg)
+
+    return {
+        'statusCode': 200,
+        'body': event
+    }
+
+"""
+        data = {
+            "replyToken": reply_token,
+            "messages":[
+                {
+                    "type":"text",
+                    "text":"完了の正規表現で拾っています"
+                }
+            ]
+        }
+        _ = requests.post(url,headers=headers, json=data)
+"""
+
+
+"""
+    elif message == "お休みです":
+        data = {
+            "replyToken": reply_token,
+            "messages":[
+                {
+                    "type":"text",
+                    "text":"お休み了解です"
+                }
+            ]
+        }
+        _ = requests.post(url,headers=headers, json=data)
+"""
+"""
+    elif message == "バドミントンです":
+        data = {
+            "replyToken": reply_token,
+            "messages":[
+                {
+                    "type":"text",
+                    "text":"バド了解です"
+                }
+            ]
+        }
+        _ = requests.post(url,headers=headers, json=data)
+"""
+
+
+def message_to_line(reply_token,return_msg):
+    headers = {'Authorization': 'Bearer '+auth}
+    data = {
+        "replyToken": reply_token,
+        "messages":[
+            {
+                "type":"text",
+                "text":return_msg
+            }
+        ]
+    }
+    _ = requests.post(url,headers=headers, json=data)
